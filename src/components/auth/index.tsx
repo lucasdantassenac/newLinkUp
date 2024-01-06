c602e16f5e36b0be9ef47168cf1f34518e3ccbd3
GabrielModesto <gabriel_26@outlook.com.br>
2024-01-06 00:43:11 -0300

    visual screen Login Registration

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react'
 import { Alert, View, Text, TouchableOpacity, Touchable, TextInput } from 'react-native'
 import { supabase } from '../../lib/supabase'
 import { Button, Input } from 'react-native-elements'
-
+import { StatusBar } from 'expo-status-bar';
 import { styles } from './styles'
 import AntDesignIcon from 'react-native-vector-icons/AntDesign'
 import EntypoIcon from 'react-native-vector-icons/Entypo'
 import { useFonts, Poppins_700Bold, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import { useNavigation } from '@react-navigation/native';
 import { StackTypes } from '../../routers/stack';
+import Icon from 'react-native-vector-icons/AntDesign';
+import StyleDesenho from './StyleDesenho';
+import navigatorBar from './../../components/stylesGlobal/navigatorBar';
+import viewColor from '../../components/stylesGlobal/myColors';
+import footer from '../../components/stylesGlobal/footer';
+import myInputs from '../../components/stylesGlobal/inputs';
+import myButtons from '../../components/stylesGlobal/myButtons';
+import myPrincipalContainer from '../../components/stylesGlobal/myPrincipalContainer';
 
 export default function Auth() {
   const [email, setEmail] = useState('')
@@ -44,65 +52,68 @@ export default function Auth() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.backButtonContainer}>
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-        >
-          <AntDesignIcon 
-            name='arrowleft'
-            size={30}
-            color='black'
-          />
-        </TouchableOpacity>
-        <Text 
-          style={styles.backButton}
-         >Voltar
-         </Text>
+
+<View>
+
+  <StyleDesenho/>
+    <View style={myPrincipalContainer.containerPrincipalCadastro}>
+
+      <View style={navigatorBar.containerNav}>
+          <TouchableOpacity style={navigatorBar.buttonback} onPress={() => navigation.navigate('ProfileUser')}>
+            <Icon name='arrowleft' size={30} style={{fontWeight:'bold', color: viewColor.padraoText2.color}} />
+            <Text style={navigatorBar.textback2}>Voltar</Text>
+          </TouchableOpacity>
       </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.pageTitle}>Entrar</Text>
-      </View>
-      <View style={styles.inputContainer} >
-        <Text style={styles.label}>Matrícula ou CPF</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@exemplo.com"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={styles.inputContainer} >
-        <Text style={styles.label}>Senha</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Sua senha"
-          autoCapitalize={'none'}
-        />
-        <View style={styles.forgetPasswordContainer}>
-          <Text style={styles.forgetPasswordText}>Esqueceu a senha?</Text>
+
+      <View style={myPrincipalContainer.subcontainerPrincipal}>
+        <View style={styles.containerTitulo}>
+          <Text style={styles.cadastroTitulo}>Entrar</Text>
+        </View>
+        
+        <View style={myInputs.containerPrincipalInputs}>
+
+          <View style={myInputs.containerInput}>
+            <Text style={myInputs.textInput}>Matrícula ou CPF</Text>
+            <TextInput style={myInputs.input}
+              onChangeText={(text) => setEmail(text)}
+              value={email}
+              placeholder="email@exemplo.com"
+              autoCapitalize={'none'}
+              />
+          </View>
+
+          <View style={myInputs.containerInput}>
+            <Text style={myInputs.textInput}>Senha</Text>
+            <TextInput style={myInputs.input}
+              onChangeText={(text) => setPassword(text)}
+              value={password}
+              secureTextEntry={true}
+              placeholder="Sua senha"
+              autoCapitalize={'none'}
+              />
+            <View style={styles.forgetPasswordContainer}>
+              <Text style={styles.forgetPasswordText}>Esqueceu a senha?</Text>
+            </View>
+          </View>
+
+          <View style={myButtons.containerButton}>
+            <TouchableOpacity 
+              onPress={() => signInWithEmail()} 
+              style={myButtons.buttonLogin}
+              >
+              <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
+          </View>
+
         </View>
       </View>
-      
-      {/* <View >
-        <Button Text="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
-      </View> */}
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity 
-          
-          onPress={() => signInWithEmail()} 
-          style={styles.button}
-          >
-            <Text style={styles.buttonText}>Entrar</Text>
-        </TouchableOpacity>
 
-        
+      <View style={footer.containerFooter}>
+          <Text style={footer.textFooter}>Copyright © 2023. Todos os direitos reservados · Privacy · Terms</Text>
       </View>
-      
+
     </View>
+    <StatusBar backgroundColor='#215151'/>
+</View>
   )
 }
--- src/components/auth/styles.tsx
+++ src/components/auth/styles.tsx
@@ -1,71 +1,54 @@
 import { StyleSheet } from "react-native";  
+import myColors from "../stylesGlobal/myColors";
 
 export const styles = StyleSheet.create({
-    container:{
-        padding:24,
-        marginTop:32
-    },
-    buttonContainer:{
 
-        alignItems:'center',
-        marginTop:32,
-        justifyContent:'center'
 
-    },
-    backButtonContainer:{
-        flexDirection:'row',
-        alignItems:'center',
-        marginBottom:32
-    },
-    titleContainer:{
-        marginBottom:32
-    },
-    inputContainer:{
-        marginBottom:32
-    },
+    containerTitulo:{
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%',
+        height: 'auto',
+      },
+        cadastroTitulo:{
+          width: 'auto',
+          height: 'auto',
+          fontSize: 40,
+          fontWeight: '600',
+          color: myColors.padraoText2.color,
+        },
+  
+        containerSubtitulo:{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: 'auto',
+        },
+  
+        subtituloPadrãoCadastro:{
+          width: '100%',
+          height: 'auto',
+          fontSize: 16,
+          fontWeight: '300',
+          color: myColors.padraoText2.color,
+        },
+
     forgetPasswordContainer:{
-        marginTop:10
+        marginTop:10,
     },
 
-    backButton:{
-        fontSize:18
+    forgetPasswordText:{
+        textAlign:'right',
+        color: myColors.padraoText2.color,
     },
-    pageTitle:{
-        color:"#504f4f",
-        fontSize:32,
-        fontWeight:'700',
-    },
-    label:{
-        fontSize:16,
-        letterSpacing:0.5,
-        marginBottom:6,
-        fontFamily:'Poppins_400Regular',
-    },
-    input:{
-        borderRadius:25,
-        borderColor:'#c9c9c9',
-        borderWidth:1,
-        padding:12
-    },
-    button:{
-        backgroundColor: '#5E5E5E',
-        color:"#fff",
-        padding: 10,
-        borderRadius: 30,
-        marginTop: 20,
-        marginBottom:10,
-        textAlign:'center',
-        maxWidth:350,
-        width:250,
-    },
+
     buttonText:{
-        color: 'white',
+        color: myColors.padraoText2.color,
         fontWeight: '400',
         fontSize: 16,
         textAlign:'center',
     },
-    forgetPasswordText:{
-        textAlign:'right'
-    }
+
 })
 
